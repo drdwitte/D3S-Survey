7ccfb6f3770e6e7e698fb6151a7db868536f39f5
Dieter De Witte <drdwitte@gmail.com>
2015-03-23 01:33:55 +0100

    Full Visualization, matrix plugged in

--- FullVisualization/scripts/Force.js
+++ FullVisualization/scripts/Force.js
@@ -11,6 +11,7 @@ function calculateAllDistances(skillset,dataset){
 		var si=skillset[i];
 		for (var j=0; j<i; j++){
 			var sj=skillset[j];
+			//console.log(si+" "+sj);
 			var dij = calculateSkillDistance(dataset,skillset[i],skillset[j]);
 			distanceMatrix[si][sj] = dij;
 			distanceMatrix[sj][si] = dij;
@@ -21,30 +22,79 @@ function calculateAllDistances(skillset,dataset){
 
 function calculateSkillDistance(data, skilli, skillj){
 
-var dist = 0.0;
-
-for (var i=0; i<data.length; i++){
-	var scorei= data[i][skilli];
-	var scorej= data[i][skillj];	
-	dist+= Math.pow(scorei-scorej,2);
+	var dist = 0.0;
+	var maxDist = Math.sqrt(data.length)*4;
+	for (var i=0; i<data.length; i++){
+		var scorei= data[i][skilli];
+		var scorej= data[i][skillj];	
+		dist+= Math.pow(scorei-scorej,2);
+	}
+	dist = Math.sqrt(dist) / maxDist;
+	return dist;
 }
 
-return Math.sqrt(dist);
+function generateFrequencyDistribution(dataset, allSkills){
 
+	
+
+
+	var mockup = [
+					{"name":"Algorithms","freqs":[55,60,59,50,17]},
+					{"name":"BackEnd","freqs":[85,67,35,36,18]},
+					{"name":"Bayesian","freqs":[88,74,84,24,7]},
+					{"name":"DistributedData","freqs":[56,87,61,26,11]},
+					{"name":"Business","freqs":[38,54,43,73,33]},
+					{"name":"ClassicalStats","freqs":[33,51,70,58,29]},
+					{"name":"DataWrangling","freqs":[34,45,53,70,39]},
+					{"name":"FrontEnd","freqs":[67,72,50,40,12]},
+					{"name":"GraphModels","freqs":[81,73,46,29,12]},
+					{"name":"MachineLearning","freqs":[51,71,42,53,24]},
+					{"name":"Math","freqs":[30,45,77,56,33]},
+					{"name":"Optimization","freqs":[54,72,66,38,11]},
+					{"name":"ProductDev","freqs":[50,48,57,55,31]},
+					{"name":"Science","freqs":[60,51,47,46,37]},
+					{"name":"Simulation","freqs":[96,67,55,20,8]},
+					{"name":"GIS","freqs":[123,70,25,18,5]},
+					{"name":"StructuredData" ,"freqs":[40,39,53,66,43]},
+					{"name":"Marketing","freqs":[74,81,50,25,11]},
+					{"name":"SysAdmin","freqs":[101,54,43,32,11]},
+					{"name":"TimeSeries","freqs":[61,65,67,34,14]},
+					{"name":"UnstructuredData","freqs":[69,61,53,41,17]},
+					{"name":"Visualization","freqs":[42,63,63,48,25]}
+				];
+
+	return mockup;
 }
 
 
-function createForceVisualization(allSkills, skillDistances){
 
-	var selectedSkills = []; //ordered array, showing which distances are currently clicked
 
-	//force Code
+
+
+function createForceVisualization(allSkills, data){
+
+	//ordered array, showing which distances are currently clicked
+	var selectedSkills = ["Algorithms","BackEnd","Bayesian","DistributedData","Business","ClassicalStats","DataWrangling","FrontEnd","GraphModels","MachineLearning","Math","Optimization",
+	"ProductDev","Science","Simulation","GIS","StructuredData","Marketing","SysAdmin","TimeSeries","UnstructuredData","Visualization"]; 
+
+
+
 	d3.select("#ForceVisualization").append("p").text("Force Visualization");
 
-	
-	
+	//generate skill distances;
+	//double map [skill1][skill2] -> euclidean distance * factor to map between 0 en 1 = 5*numPeople; 
+	var skillDistances = calculateAllDistances(allSkills,data);
+	//console.log(skillDistances);
+
+	var freqDistr = generateFrequencyDistribution(data, allSkills);
+
+	//***
+	//force Code
+	//***
+
 
 	//bind histogram matrix to click event
-	createHistogramMatrix(selectedSkills);
+	createHistogramMatrix(selectedSkills,data, freqDistr);
+
 }
 
--- FullVisualization/scripts/Hist.js
+++ FullVisualization/scripts/Hist.js
@@ -1,12 +1,212 @@
+	
+function createHistogramMatrix(activeSkills, dataset, freqDistrSkills){
 
-function createHistogramMatrix(activeSkills){
 	//histogram Code
-	d3.select("#HistogramMatrixVisualization").append("p").text("HMatrix Visualization");
+	var div = d3.select("#HistogramMatrixVisualization");
+	
+
+	//create svg
+	div.selectAll("svg").remove();
+	var svg = div.append("svg")		
+			.attr("width",stripPX(div.style("width")))
+			.attr("height",stripPX(div.style("height")))
+			;
+	var canvas = {svg:svg, margin:{top:20,bottom:5,left:60,right:60}};
+
+	//filter the frequency distribution for the active skills
+	var filteredDistr = filterFreqDistribution(freqDistrSkills,activeSkills);
+	
+	//filterDictionary contains for each skill the cutoff value to filter individuals	
+	var filterDictionary = initializeFilterDictionary(activeSkills);	
+
+
+	drawHistogramMatrix(freqDistrSkills, canvas, filterDictionary);
+
+	//convert the input dataset to a format where the keys are explicitely in the value of the fields
+	var convertedCSV = convertCSV(dataset,activeSkills);
 	
 	//bind population
-	createPopulationVisualization();	
+	createPopulationVisualization(convertedCSV);
+	createStaticPopulationVisualization(convertedCSV);	
 	//bind parallel coordinates
 	createPCVisualization();
+}
+
+function initializeFilterDictionary(activeSkills){
+	var filter = {};
+	
+	for (var i=0; i<activeSkills.length; i++){
+		filter[activeSkills[i]]=0;
+	}
+	return filter;
+
+}
+
+function drawHistogramMatrix(data, canvas, filter){
+
+	var numBoxes = data.length;
+	var totalBoxWidth = canvas.svg.attr("width") - canvas.margin.left - canvas.margin.right;
+	var singleBoxRange = totalBoxWidth / numBoxes;
+	var singleBoxWidth = singleBoxRange*0.80;
+
+	var left = canvas.margin.left;
+
+	for (var i=0; i<numBoxes; i++)
+	{	
+		drawOneHistogram(data[i]["freqs"], data[i].name,left+i*singleBoxRange,left+i*singleBoxRange+singleBoxWidth, canvas, filter);
+	}
+
+	
+}
+
+function drawOneHistogram(data, dataName, xLeft, xRight, canvas, filter){
+
+	var xScale = d3.scale.linear()
+	    		.domain([0, d3.max(data)])
+	    		.range([xLeft,xRight]);
+
+	var yScale = d3.scale.ordinal()
+	    		.domain([1,2,3,4,5])
+	    		.rangeBands([canvas.svg.attr("height")-canvas.margin.bottom,canvas.margin.top]);
+
+	var bars = canvas.svg.selectAll("bars")
+	  		.data(data)
+	  		.enter()
+			.append("rect")
+	    			.attr("width"  , function(d)  { return xScale(d)-xScale(0);})
+				.attr("height" , function(d)  { return yScale.rangeBand()*0.95;})
+				.attr("x"      , function(d)  { return xScale(0);})
+				.attr("y"      , function(d,i){ return yScale(i+1);})
+				.attr("class",dataName)
+				.attr("id", function(d,i){return i+1;})
+				.style("fill" ,  "green")
+				.on("click",function(d,i){
+					/*	
+					De class voor de elementen wordt de dataName (staat hier enkele regels boven)
+					en de id is de score die ze gegeven hebben (1-5). Je kan dan de geslecteerde id gebruiken
+					als uiterste en alle bars met een id die minder is (of meer indien we willen) een andere
+					style geven.
+					*/
+					var classname = "." + dataName;
+					
+					//When selecting d3.selectAll by classname, it returns an array object, with 1 array at index 0.
+					// I am also setting 'clearing the previous selection' (if there was any) by giving all selected
+					// elements back their red fill. You can read up on selections here: https://github.com/mbostock/d3/wiki/Selections
+					var selectionObject = d3.selectAll(classname).attr("style","fill:green");
+					var selection = selectionObject[0];
+
+					for ( j=0; j<i; j++){
+						selection[j].setAttribute("style","fill:red");
+					}
+					filter[dataName]=i;
+
+					//drawSlopeDiagram();
+					
+				})
+			;
+
+	var text = canvas.svg.selectAll("barlabels")
+			.data(data)
+			.enter()
+			.append("text")
+				.attr("x"      , function(d)  { return Math.max(xScale(0.5),xScale(d)-25);})
+				.attr("y"      , function(d,i){ return yScale(i+1)+yScale.rangeBand()/2;})
+				.text(function(d){ return d})
+				.style("fill","white")
+				;
+
+	
+	generateAxes(xScale,yScale,canvas,xLeft,dataName);
+
+
+		
+}
+
+function generateAxes(xScale,yScale,canvas,xLeft,caption){
+
+	//drawing the X and Y-axis + add a caption
+	var xAxis = d3.svg.axis()
+			.scale(xScale)
+			.orient("top")
+				.ticks(0)
+					;
+
+	var yAxis = d3.svg.axis()
+				.scale(yScale)
+				.orient("left")
+				.ticks(5)
+				;
+
+	var gX = canvas.svg.append("g");
+	gX.attr("class","axis");
+	gX.attr("transform","translate("
+					+0
+					+","
+					+canvas.margin.top
+					+")"
+		);
+	gX.call(xAxis);
+
+	var gY = canvas.svg.append("g");
+	gY.attr("class","axis");
+	gY.attr("transform","translate("
+					+xLeft
+					+","
+					+0
+					+")"
+			);
+	gY.call(yAxis);
+
+	canvas.svg.append("text")
+		.text(caption)
+		.attr("transform","translate("
+					+xLeft
+					+","
+					+canvas.margin.top/2
+					+")"
+		);
+
+}
+
+
+
+
+
+function convertCSV(csvObject, active){
+	var converted = [];
+	for (var i=0; i<csvObject.length; i++){
+		converted.push(convertSingleRec(csvObject[i],active));	
+	}
+	return converted;
+}
+
+function convertSingleRec(record, active){
+
+	var converted = {};
+	converted["visible"]=false;
+	converted["skills"] =[];
+	for (var i=0; i<active.length; i++){
+		var sc = record[active[i]];
+		converted["skills"].push({type:active[i], score:sc});	
+	}
+	return converted;
+	
+}
+
+
+
+function filterFreqDistribution(elements,names){
+
+	var filtered = [];
+
+	for (var i=0; i<elements.length; i++){
+		//console.log(elements[i].name);
+		
+		if (names.indexOf(elements[i].name)>=0)
+			filtered.push(elements[i]);		
+	} 
+
+	return filtered;
 }
 
 
